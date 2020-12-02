a3453920313ac3c502fe29b17e71b0c72a937adb
Cagri Babuccuoglu <cagribabuccuoglu@gmail.com>
2020-12-02 11:07:36 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,544 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+
+		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
+		Remove this if you use the .htaccess -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+		<title>Herkül Makina</title>
+		<meta name="description" content="">
+		<meta name="author" content="Laze">
+
+		<meta name="viewport" content="width=device-width; initial-scale=1.0">
+		
+		<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+		<!-- Owl carousel-->
+        <link rel="stylesheet" href="css/owl.carousel.css">
+       
+        <link rel="stylesheet" href="css/owl.theme.default.css">
+       <link rel="stylesheet" href="css/style_hero.css">
+		<link rel="stylesheet" type="text/css" href="css/hover.css">
+		<link rel="stylesheet" type="text/css" href="css/animate.css">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+		<link rel="stylesheet" type="text/css" href="css/style.css?2"/>
+		<link rel="stylesheet" type="text/css" href="css/responsive%20.css"/>
+
+		<!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
+		<link rel="shortcut icon" href="img/favicon.png">
+	</head>
+
+	<body>
+	
+	<header id="main-header" class="two">
+			
+				
+				<div class="top-line-two">
+                  <div class="container">
+					<div class="row">
+						<div class="col-md-9 col-sm-6">
+							<div class="welcome">
+								<ul>
+									<li><i class="fa fa-phone" aria-hidden="true"></i>Tel: +90 (312) 000 00 00 </li>
+									<li><i class="fa fa-envelope" aria-hidden="true"></i>Mail@herkulmakina </li>
+									<li><i class="fa fa-globe" aria-hidden="true"></i>Çankaya / Ankara</li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-6">
+							<div class="social-icons">
+								<ul>
+									<li>
+										<a href="#" ><i class="fa fa-facebook" aria-hidden="true"></i></a>
+									</li>
+									<li>
+										<a href="#" ><i class="fa fa-twitter" aria-hidden="true"></i></a>
+									</li>
+
+									<li>
+										<a href="#" ><i class="fa fa-instagram" aria-hidden="true"></i></a>
+									</li>
+
+								</ul>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			
+			</div>
+			
+			<section class="main-menu main-menu-two">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3">
+						<div class="logo">
+							<a href="#"><img src="img/logo.png" style="width: 200px" /> </a>
+						</div>
+					</div>
+					<div class="col-md-9">
+						<nav id="main-navigation-wrapper" class="navbar navbar-default">
+							<div class="navbar-header">
+								<button type="button" data-toggle="collapse" data-target="#main-navigation" aria-expanded="false" class="navbar-toggle collapsed">
+									<span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+								</button>
+							</div>
+							<div id="main-navigation" class="collapse navbar-collapse">
+								<ul class="nav navbar-nav navbar-right" style="margin-top: 10px;">
+
+                                    <li>
+										<a href="#">Anasayfa</a>
+									</li>
+                                    <li><a href="#">Hakkımızda</a></li>
+                                    <li>
+                                        <a href="#">FİRMALAR</a>
+                                        <ul class="dropdown-submenu">
+											<li><a href="#">Makine Grubu</a></li>
+											<li><a href="#">Kalıp Grubu</a></li>
+											<li><a href="#">Endüstriyel Yağlar Grubu</a></li>
+										</ul>
+                                    </li>                                                                             
+									<li><a href="#">Duyurular</a></li>
+                                    <li><a href="#">Referanslar</a></li>
+                                    <li><a href="#">VİDEOLAR</a></li>
+                                    <li><a href="#">İLETİŞİM</a></li>                                               
+                                        
+						
+								</ul>
+							</div>
+						</nav>
+					</div>
+					
+				</div>
+			</div>
+		</section>
+			
+			
+		</header>
+	
+
+	<!-- Header slider -->
+		<section class="cd-hero">
+			<ul class="cd-hero-slider autoplay">
+
+				<li class="move-left">
+					<div class="cd-full-width">
+						<div class="container wth">
+							<h2>
+							
+							We help Industries <br><span class="color-default">Inovate and Grow</span></h2>
+							<p>
+								Finding your next Financial Advisor is as easy as counting from one to five.
+							</p>
+							<a data-animation="animated fadeInLeft" href="#" class="thm-button hvr-bounce-to-right">Learn more</a>
+                        <a data-animation="animated fadeInRight" href="#" class="thm-button inverse hvr-bounce-to-right2">View services</a>
+						</div>
+					</div>
+				</li>
+
+				<li class="move-left">
+					<div class="cd-full-width">
+						<div class="container text-left">
+							<h2>Leader
+							
+							<span class="color-default">in Power<br> and Automotion</span></h2>
+							<p class="color-white">
+								Etiam iaculis rhoncus velit, sit amet aliquet sem euismod at
+								<br>
+								integer consequat velit ac ultrices commodo.
+							</p>
+							<a data-animation="animated fadeInLeft" href="#" class="thm-button hvr-bounce-to-right">Learn more</a>
+                           <a data-animation="animated fadeInRight" href="#" class="thm-button inverse hvr-bounce-to-right2">View services</a>
+						</div>
+					</div>
+				</li>
+
+				<li class="selected from-right">
+					<div class="cd-full-width">
+						<div class="container text-left pdd">
+							<h2 class="color-white">Welcome <span class="color-default">to all<br> Industrial Business</span></h2>
+							<p>
+								Etiam iaculis rhoncus velit, sit amet aliquet sem euismod at
+								<br>
+								aliquet sem integer consequat velit ac ultrices commodo.
+							</p>
+							<a data-animation="animated fadeInLeft" href="#" class="thm-button hvr-bounce-to-right">Learn more</a>
+                  <a data-animation="animated fadeInRight" href="#" class="thm-button inverse hvr-bounce-to-right2">View services</a>
+						</div>
+					</div>
+				</li>
+
+			
+
+			</ul>
+
+			<div class="cd-slider-nav">
+				<nav class="container">
+					<span class="cd-marker item-3"></span>
+
+					<ul>
+						<li class="">
+							<a href="#0">
+							<div class="slide-number">
+								<img src="img/icon-slider1.png" />
+							</div>
+							<div class="text-box">
+							<p>
+								Quality Work Process
+							</p><span>The quality fork is Our Priority</span>
+							</div></a>
+						</li>
+						<li class="">
+							<a href="#0">
+							<div class="slide-number">
+								<img src="img/icon-slider2.png" />
+							</div>
+							<div class="text-box">
+							<p>
+								Team Job
+							</p><span>We have 15 Years Experience</span></div></a>
+						</li>
+						<li class="selected">
+							<a href="#0">
+							<div class="slide-number">
+								<img src="img/icon-slider3.png" />
+							</div>
+							<div class="text-box">
+							<p>
+								Testing and Executing
+							</p><span>Doing the right thing</span></div></a>
+						</li>
+						
+					</ul>
+				</nav>
+			</div>
+
+		</section>
+		<section class="about-us-third">
+			<div class="container">
+			  <div class="row">
+			  	<div class="col-md-6">
+			  		<div class="worker">
+			  			<img src="img/worker.png" />
+			  		</div>
+			  	</div>
+			  	<div class="col-md-6">
+			  		<div class="title">
+			  			<h2>Hakkımızda</h2>
+			  		</div>
+			  		<div class="text-box">
+			  			<p><b>HERKÜL ENDÜSTRİYEL PAKETLEME VE MAKİNA SANAYİ</b>, 2010 yılında Sn. Salim Sarıgül tarafından İstanbul da kurulmuştur. Kendi alanında marka olmuş birçok Avrupa ve Tayvan menşeli firmanın Türkiye temsilciliğini yapmaktadır.</p>
+			  			<p>Bağlantı elemanları sektöründe faaliyet göstermekte olan müşterilerimize, üretimlerinin her aşamasında ihtiyaç duydukları her türlü makine, kalıp ve teknolojik destek konusunda Hizmet sunmaktadır. </p>
+			  		</div>
+			  	</div>
+			  </div>		
+			</div>
+		</section>
+	 
+		<section class="our-projects-three">
+			<div class="container">
+			    <div class="row head">
+			    	<div class="col-md-12">
+			        <div class="title">
+			        <h2>Makine ve Kalıplar</h2>
+			        </div>
+			        </div>
+			    </div>
+				
+			</div>
+			<div class="full-width-slider">
+				<div id="our-projects" class="owl-carousel owl-theme"> 
+					<div class="item">
+						<div class="image">
+							<img class="img-responsive" src="img/project-two1.jpg" alt="Nutrition Photo">
+							<div class="overlay">
+								<div class="button"><a class="btn" href="#">İNCELE</a></div>
+								<div class="text">
+									
+									<h4>Power and Energy</h4>
+									
+								</div>
+							</div>
+						</div>								
+					</div>
+					<div class="item">
+						<div class="image">
+							<img class="img-responsive" src="img/project-two2.jpg" alt="Nutrition Photo">
+							<div class="overlay">
+								<div class="button"><a class="btn" href="#">İNCELE</a></div>
+								<div class="text">
+									<h4>Power and Energy</h4>
+								</div>
+							</div>
+						</div>								
+					</div>
+					<div class="item">
+						<div class="image">
+							<img class="img-responsive" src="img/project-two3.jpg" alt="Nutrition Photo">
+							<div class="overlay">
+								<div class="button"><a class="btn" href="#">İNCELE</a></div>
+								<div class="text">
+									<h4>Power and Energy</h4>
+								</div>
+							</div>
+						</div>								
+					</div>
+					<div class="item">
+						<div class="image">
+							<img class="img-responsive" src="img/project-two4.jpg" alt="Nutrition Photo">
+							<div class="overlay">
+								<div class="button"><a class="btn" href="#">İNCELE</a></div>
+								<div class="text">
+									<h4>Power and Energy</h4>
+								</div>
+							</div>
+						</div>								
+					</div>
+					<div class="item">
+						<div class="image">
+							<img class="img-responsive" src="img/project-two5.jpg" alt="Nutrition Photo">
+							<div class="overlay">
+								<div class="button"><a class="btn" href="#">İNCELE</a></div>
+								<div class="text">
+									<h4>Power and Energy</h4>
+								</div>
+							</div>
+						</div>								
+					</div>
+					<div class="item">
+						<div class="image">
+							<img class="img-responsive" src="img/project-two5.jpg" alt="Nutrition Photo">
+							<div class="overlay">
+								<div class="button"><a class="btn" href="#">İNCELE</a></div>
+								<div class="text">
+									<h4>Power and Energy</h4>
+								</div>
+							</div>
+						</div>								
+					</div>
+					<div class="item">
+						<div class="image">
+							<img class="img-responsive" src="img/project-two5.jpg" alt="Nutrition Photo">
+							<div class="overlay">
+								<div class="button"><a class="btn" href="#">İNCELE</a></div>
+								<div class="text">
+									<h4>Power and Energy</h4>
+								</div>
+							</div>
+						</div>								
+					</div>
+				</div>
+			</div>
+		</section>
+		
+		
+		
+		
+		<section class="latest-news-third">
+			<div class="container">
+				<div class="row">
+					<div class="title">
+						<h2>Son Duyurular</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-4">
+						<div class="item">
+							<div class="img-box">
+								<img src="img/latest%20news1.jpg" />
+								<div class="date">
+									<p>14 <br>May</p>
+								</div>
+							</div>
+							<div class="text-box">
+								<a href="#"><h4>Here is Why Our new Factory will Change the Future</h4></a>
+								<p><span>by Richard | <a href="#">14 Comments</a></span></p>
+								<p>Morbi tincidunt iaculis ipsum eget laoreet. Cras at finibus ante Mauris sollicitudin ornare ullamcorper. Donec in arcu purus. </p>
+								<a href="#">See More</a>
+							</div>
+						</div>
+					</div>
+						<div class="col-md-4">
+						<div class="item">
+							<div class="img-box">
+								<img src="img/latest%20news2.jpg" />
+								<div class="date">
+									<p>14 <br>May</p>
+								</div>
+							</div>
+							<div class="text-box">
+								<a href="#"><h4>Here is Why Our new Factory will Change the Future</h4></a>
+								<p><span>by Richard | <a href="#">14 Comments</a></span></p>
+								<p>Morbi tincidunt iaculis ipsum eget laoreet. Cras at finibus ante Mauris sollicitudin ornare ullamcorper. Donec in arcu purus. </p>
+								<a href="#">See More</a>
+							</div>
+						</div>
+					</div>
+						<div class="col-md-4">
+						<div class="item">
+							<div class="img-box">
+								<img src="img/latest%20news3.jpg" />
+								<div class="date">
+									<p>14 <br>May</p>
+								</div>
+							</div>
+							<div class="text-box">
+							<a href="#">	<h4>Here is Why Our new Factory will Change the Future</h4></a>
+								<p><span>by Richard | <a href="#">14 Comments</a></span></p>
+								<p>Morbi tincidunt iaculis ipsum eget laoreet. Cras at finibus ante Mauris sollicitudin ornare ullamcorper. Donec in arcu purus. </p>
+								<a href="#">See More</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="latest-news-third">
+			<div class="container">
+				<div class="row">
+					<div class="title">
+						<h2>Temsilcilik Ağımız</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<img src="img/world-map.png" style="width: 100%" />
+					</div>
+					
+				</div>
+			</div>
+		</section>
+
+		<section class="partners">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+					<h2>Temsilciliklerimiz</h2>
+					</div>
+					<div class="col-md-12">
+						<div id="about-us-our-partners" class="owl-carousel owl-theme"> 
+						<div class="item-partner">
+								<div class="image">
+									<img class="img-responsive" src="img/logo.jpg" alt="Nutrition Photo">
+								</div>
+								
+							</div>
+							<div class="item-partner">
+								<div class="image">
+									<img class="img-responsive" src="img/logo.jpg" alt="Nutrition Photo">
+								</div>
+								
+							</div>
+							<div class="item-partner">
+								<div class="image">
+									<img class="img-responsive" src="img/logo.jpg" alt="Nutrition Photo">
+								</div>
+								
+							</div>
+							<div class="item-partner">
+								<div class="image">
+									<img class="img-responsive" src="img/logo.jpg" alt="Nutrition Photo">
+								</div>
+								
+							</div>
+							<div class="item-partner">
+								<div class="image">
+									<img class="img-responsive" src="img/logo.jpg" alt="Nutrition Photo">
+								</div>
+								
+							</div>
+					</div>	
+					</div>
+				</div>
+			</div>
+		</section>
+		<footer class="main-footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3">
+						<figure>
+							<img class="img-responsive" src="img/logo.png" alt="awsome image" />
+						</figure>
+
+						<div class="social-icons">
+						<ul>
+							<li>
+									<a href="#" ><i class="fa fa-facebook" aria-hidden="true"></i></a>
+								</li>
+								<li>
+									<a href="#" ><i class="fa fa-twitter" aria-hidden="true"></i></a>
+								</li>
+																
+								<li>
+									<a href="#" ><i class="fa fa-instagram" aria-hidden="true"></i></a>
+								</li>
+								<li>
+									<a href="#" ><i class="fa fa-youtube" aria-hidden="true"></i></a>
+								</li>
+								
+						</ul>
+					</div>
+					</div>
+					<div class="col-md-3 sec-tab">
+						<h2>Menü</h2>
+						<ul class="solutions" style="margin-top: 0px">
+                         <li><a href="#">Anasayfa</a></li>
+                         <li><a href="#">Hakkımızda</a></li>
+                         <li><a href="#">Firmalar</a></li>
+                         <li><a href="#">Duyurular</a></li>
+                         <li><a href="#">Referanslar</a></li>
+                         <li><a href="#">Videolar</a></li>
+                         <li><a href="#">İletişim</a></li>
+						</ul>
+					</div>
+					<div class="col-md-3">
+						<h2>Son Duyurular</h2>
+						<div class="footer-news">
+							<p>
+								Fusce non faucibus lorem. Cras eu velit id diam cursus tincidunt in ut dui. Quisque quis augue placerat.
+								<br>
+								<span>July 12, 2016</span>
+							</p>
+							<p>
+								Fusce non faucibus lorem. Cras eu velit id diam cursus tincidunt in ut dui.
+								<br>
+								<span>July 12, 2016</span>
+							</p>
+							<p>
+								Fusce non faucibus lorem. Cras eu velit id diam cursus tincidunt int.
+								<br>
+								<span>July 12, 2016</span>
+							</p>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<h2>İletişim</h2>
+						<ul class="contact-info">
+                         <li><i class="fa fa-map-marker" aria-hidden="true"></i><div class="address"> Bristol Town 2578 AGS,<br> New York City</div></li>
+                         <li><i class="fa fa-phone" aria-hidden="true"></i><div class="address">(018) 65 524 8503 <br> (125) 954 7854</div></li>
+                         <li><i class="fa fa-clock-o" aria-hidden="true"></i><div class="address">Mon to Fri - 08:00 to 16:30 </div></li>
+                         <li><i class="fa fa-envelope" aria-hidden="true"></i><div class="address"><a href="#">contact@factory.com  </a></div></li>
+                         
+						</ul>
+					</div>
+					</div>
+					</div>
+					</footer>
+					
+					<a class="scroll-top" href="#"><i class="fa fa-angle-up"></i> </a>
+		<!-- Scripts -->
+		
+		<script src="plugins/jquery.min.js"></script>
+		<script src="plugins/owl.carousel.min.js"></script>
+		 <script src="../../cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
+		<script src="plugins/bootstrap.min.js"></script>
+		<script src="OwlCarousel/dist/owl.carousel.min.html"></script>
+		<script src="plugins/waypoints.min.js"></script>
+		<script src="plugins/jquery.counterup.min.js"></script>
+		<script src="plugins/theme.js"></script>
+		
+		
+	</body>
+</html>
